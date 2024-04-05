5da1e9d052a45adcf02faea65427bde566ea36b8
Ahmedwagdymohy <ahmedwagdymohy@gmail.com>
2024-04-06 00:34:40 +0200

    finishin the Queue implemention

--- Queue.cpp
+++ Queue.cpp
@@ -29,7 +29,7 @@ struct queue{
 template <typename T> 
 struct queue <T> createQueue(){
     struct queue<T> Queue;
-    
+    /**we can replace the using of the pointers by using the functions of the vector*/
     Queue.firstPointer  = &(Queue.queueData.front());
     Queue.secondPointer = &(Queue.queueData.back());
     return Queue;
@@ -38,16 +38,40 @@ struct queue <T> createQueue(){
 
 template <typename T> 
 void queueElement(struct queue<T> &Queue, T data){
+    /**adding element in the back of the queue*/
+    Queue.queueData.push_back(data);
     
+}
+
+template <typename T> 
+void dequeueElement(struct queue<T> &Queue){
+    /**removing the first elemet of the queue*/ 
+    Queue.queueData.erase(Queue.queueData.begin());
+
 }
 
 
 
+template<typename T>
+void printElements(struct queue<T> &Queue){
+    int iteration_number = 0;
+    for(int i: Queue.queueData){
 
-
+        cout<<"the "<<iteration_number+1<<" element of the array is "<<i<<endl;
+        iteration_number++;
+    }
+}
 
 
 int main(){
+    struct queue<int> queue1 = createQueue<int>();
+    queueElement<int>(queue1 , 1);
+    queueElement<int>(queue1 , 2);
+    queueElement<int>(queue1 , 3);
+    printElements(queue1);
+    dequeueElement(queue1);
+    printElements(queue1);
+    
 
     return 0 ;
 }
